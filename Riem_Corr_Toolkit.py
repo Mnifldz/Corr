93bd1544919eee8269ffca0e7615834ea2cd52dc
Paul David <paulalexanderdavid@gmail.com>
2018-09-12 15:45:08 -0700

    Initialize Riemannian correlation toolkit

---
+++ Riem_Corr_Toolkit.py
@@ -0,0 +1,7 @@
+# Riemannian Correlation Toolkit
+#------------------------------------------------------------------------------
+# Last Updated: 9/12/2018
+# This file contains my main functions for computations on the correlation 
+# manifold under the quotient structure SPD(n)/Diag_+(n) as described in my 
+# various writings.  My functions will be here so that I no longer have to refer
+# to my Matlab implementations.
