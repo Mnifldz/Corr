ea5412b58812c74252aba76e41ff83d9feec9725
Paul David <paulalexanderdavid@gmail.com>
2018-09-18 14:48:48 -0700

    Added Correlation Newton's method (Needs Editing)

--- Riem_Corr_Toolkit.py
+++ Riem_Corr_Toolkit.py
@@ -1,6 +1,6 @@
 # Riemannian Correlation Toolkit
 #------------------------------------------------------------------------------
-# Last Updated: 9/13/2018
+# Last Updated: 9/18/2018
 # This file contains my main functions for computations on the correlation 
 # manifold under the quotient structure SPD(n)/Diag_+(n) as described in my 
 # various writings.  My functions will be here so that I no longer have to refer
@@ -13,7 +13,7 @@ import numpy.linalg as npl
 import scipy.linalg as LA
 
 
-# Distance Functions
+# Distance Functions and Miscellaneous Tools
 #------------------------------------------------------------------------------
 def SPD_Inner(base, V1, V2):
     """Returns the inner product with respect to the affine-invariant metric
@@ -25,6 +25,26 @@ def SPD_Norm(base, V):
        the inner product of a vector with itself."""
     return np.sqrt( SPD_Inner(base, V,V) )   
 
+def Dup_Ind(r,c,n):
+    """This is a helper function to 'Duplication(n)' that finds the right indexing."""
+    return int(r*n + c - r*(r+1)/2)
+
+def Duplication(n):
+    """This function produces the duplication matrix and Moore-Penrose inverse.
+       These matrices are used for computation of inverting the Hessian and 
+       solving the Newton step."""
+    N = int(n*(n+1)/2)
+    Dup = np.zeros([n*n,N])
+    for i in range(n*n):
+        row = int(np.floor(i/n))
+        col = np.mod(i,n)
+        if row <= col:
+           Dup[i, Dup_Ind(row,col,n)] = 1
+        else:
+            Dup[i, Dup_Ind(col,row,n)] = 1
+    
+    return Dup, npl.pinv(Dup)
+
 
 # Manifold Tools
 #------------------------------------------------------------------------------
@@ -35,7 +55,7 @@ def Corr_Proj(P):
     return LA.solve(Diag, np.transpose( LA.solve(Diag, P )) )
 
 
-# Descent Tools
+# SPD Gradient Descent/Newton Tools
 #------------------------------------------------------------------------------
 def SPD_Geo(base, V):
     """Returns the SPD matrix obtained by following the geodesic starting at
@@ -45,6 +65,11 @@ def SPD_Geo(base, V):
     SQRT = LA.sqrtm(base)
     SOLVE = np.transpose( LA.solve(SQRT, V )) # NOTE: This only works because we have symmetric matrices.
     return np.matmul(  np.matmul(SQRT, LA.expm( LA.solve(SQRT,  SOLVE  ))),    SQRT) 
+
+def SPD_Tan(base, Samps):
+    """Compute tangent vector based on mean-squared objective function."""
+    pre_tan = [LA.logm( LA.solve(Samps[i], base)  ) for i in range(len(Samps))]
+    return np.matmul(base, sum(pre_tan))
 
 def SPD_Hess(start, end):
     """This function computes the approximate Hessian of the mean-squared objective
@@ -60,6 +85,33 @@ def SPD_Hess(start, end):
    
     return term_1 + term_2 + term_3
 
+def SPD_Newton(Samps, thresh):
+    """This algorithm performs a Newton's method on the manifold of symmetric
+       positive-definite matrices.  We assume that 'Samps' is a list of SPD
+       matrices and 'thresh' is the maximum tolderance for the length of the 
+       final tangent vector.  This is used as a subroutine of Newton's algorithm
+       on the correlation matrices."""
+    
+    k = len(Samps)
+    n = Samps[0].shape[0]
+    # Initialize base point
+    P_opt      = sum(Samps)/k
+    pre_tan    = SPD_Tan(P_opt, Samps)
+    multi_Hess = [SPD_Hess(P_opt, Samps[i]) for i in range(k)]
+    tan        = np.reshape(  LA.solve(sum(multi_Hess), np.reshape(pre_tan, [k**2, 1] ) )  , [k,k])
+    
+    iters = 0
+    while SPD_Norm(P_opt, tan) > thresh:
+        P_opt       = SPD_Geo(P_opt, -tan)
+        pre_tan     = SPD_Tan(P_opt, Samps)
+        multi_Hess  = [SPD_Hess(P_opt, Samps[i]) for i in range(k)]
+        tan         = np.reshape(  LA.solve(sum(multi_Hess), np.reshape(pre_tan, [n**2, 1] ) )  , [n,n])
+        iters      += 1
+    
+    return P_opt, iters, SPD_Norm(P_opt, tan)
+
+# Diag_+ Gradient Descent/Newton Tools
+#------------------------------------------------------------------------------
 def Diag_Hess(C1, C2, D):
     """This is the Hessian of the diagonal manifold Diag_+(n) in order to perform
        fiber optimization."""
@@ -114,19 +166,59 @@ def Diag_Newton(C1, C2, thresh):
     # Initialize tangent vector   
     tan = Diag_Tan(C1, C2, D_opt)
     
+    Dup, Dup_inv = Duplication(C2.shape[0])
+    
     iters = 0
     while SPD_Norm(D_opt, tan) > thresh:
-        #print(str(SPD_Norm(D_opt, tan)))
         D_opt = Diag_Geo(D_opt, -tan)
         grad   = Diag_Tan(C1, C2, D_opt)
-        Hess   = Diag_Hess(C1, C2, D_opt)
+        Hess   = np.matmul( np.matmul(Dup, np.transpose(Dup)),  LA.solve(Diag_Hess(C1, C2, D_opt), np.matmul(Dup, Dup_inv) )  )
         
-        tan = np.multiply( np.identity(n), np.reshape(  LA.solve(Hess, np.reshape(grad, [n**2, 1]) )   , [n,n]) )
+        tan = np.multiply( np.identity(n), np.reshape(  np.matmul(Hess, np.reshape(grad, [n**2, 1]) )   , [n,n]) )
+        #print(tan)
+        print(SPD_Norm(D_opt, tan))
         
         iters += 1
-    
     P_opt = np.matmul(D_opt, np.matmul(C2, D_opt))    
     return D_opt, P_opt, iters, SPD_Norm(D_opt, tan)
+
+# Corr Newton
+#------------------------------------------------------------------------------
+def Corr_Newton(Samps, thresh):
+    """A Newton algorithm for correlation matrices based on the quotient 
+       manifold structure and the affine-invariant Riemannian metric adapted
+       for this subspace.  Computations are based off of SPD(n) and adapted
+       where necessary."""
+    
+    k = len(Samps)
+    n = Samps[0].shape[0]
+    # Obtain Duplication Matrix
+    Dup, Dup_inv = Duplication(n)
+    DupT         = np.transpose(Dup)
+    
+    # Initialize base point
+    Opt_Corr = np.identity(n)
+    tan      = np.zeros([n,n])
+    tan[1,0] = 0.1
+    tan[0,1] = 0.1
+    
+    iters = 0
+    while SPD_Norm(Opt_Corr, tan) > thresh:
+        #print(str(SPD_Norm(Opt_Corr, tan)))
+        print(str(SPD_Norm(Opt_Corr, tan)))
+        
+        Opt_P      = SPD_Geo(Opt_Corr, -tan)
+        Opt_Corr   = Corr_Proj(Opt_P)
+        
+        multi_SPD  = [Diag_Grad(Opt_Corr, Samps[i], 0.01, thresh)[1] for i in range(k)]
+        pre_tan    = np.matmul(Opt_Corr, sum([LA.logm( LA.solve(multi_SPD[i], Opt_Corr) ) for i in range(k) ] ) )
+        Hess       = sum([SPD_Hess(Opt_Corr, Samps[i] ) for i in range(k)] )
+        Hess       = np.matmul(  np.transpose( LA.solve(Hess, np.matmul(Dup, DupT) ) ),  np.matmul(Dup, Dup_inv)    )
+        tan        = np.reshape(  np.matmul(Hess,  np.reshape(pre_tan, [n**2,1]  ))  , [n,n])
+        iters     += 1
+    
+    
+    return Opt_Corr, iters, SPD_Norm(Opt_Corr, tan)
        
        
 # Simulation and Verification
@@ -148,7 +240,9 @@ def Corr_Simulation(n, thresh):
     C2 = Corr_Proj( LA.expm((B + np.transpose(B))/2 )  )
     
     #_, P_opt, _, _ = Diag_Grad(C1, C2, step, thresh)
-    D_opt, P_opt, _, _ = Diag_Newton(C1, C2, thresh)
+    #D_opt, P_opt, _, _ = Diag_Newton(C1, C2, thresh)
+    D_opt, P_opt, _, _ = Diag_Grad(C1, C2, 0.01,thresh)
+    
     
     P_sqrt = LA.sqrtm(P_opt)
     tan = np.matmul(P_sqrt,  np.matmul(  LA.logm( LA.solve(P_sqrt,  np.transpose(LA.solve(P_sqrt, C1  )) ) )  , P_sqrt  ))
